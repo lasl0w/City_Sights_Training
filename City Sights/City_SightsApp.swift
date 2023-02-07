439fb3fdca6aa2bfba88e228aa9c4a4af6df5a9b
Tom Montgomery <montgomery3681@gmail.com>
2023-02-07 15:30:37 -0700

    Initial Commit

---
+++ City Sights/City_SightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_SightsApp.swift
+//  City Sights
+//
+//  Created by tom montgomery on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct City_SightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ City Sights/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  City Sights
+//
+//  Created by tom montgomery on 2/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
